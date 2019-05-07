a62c14979e59a877f2e8633632b39cce33a2213b
xiamengbing <xiamengbing@192.168.0.167>
2019-05-07 11:46:15 +0800

    修改

--- dubbo-demo-api-provider/src/main/java/org/apache/dubbo/demo/provider/Application.java
+++ dubbo-demo-api-provider/src/main/java/org/apache/dubbo/demo/provider/Application.java
@@ -1,22 +1,24 @@
 package org.apache.dubbo.demo.provider;
+
+import java.util.concurrent.locks.LockSupport;
+
 import org.apache.dubbo.config.ApplicationConfig;
 import org.apache.dubbo.config.RegistryConfig;
 import org.apache.dubbo.config.ServiceConfig;
-import org.apache.dubbo.container.Main;
 import org.apache.dubbo.demo.DemoService;
 
 public class Application {
-    /**
-     */
-    public static void main(String[] args) throws Exception {
-        ServiceConfig<DemoServiceImpl> service = new ServiceConfig<>();
-        service.setApplication(new ApplicationConfig("dubbo-demo-api-provider"));
-        service.setRegistry(new RegistryConfig("zookeeper://192.168.0.167:2181"));
-        service.setInterface(DemoService.class);
-        service.setRef(new DemoServiceImpl());
-        service.export();
-        System.in.read();
-        System.in.read();
-        System.in.read();
-    }
+	public static void main(String[] args) throws Exception {
+		new Thread() {
+			public void run() {
+				ServiceConfig<DemoServiceImpl> service = new ServiceConfig<>();
+				service.setApplication(new ApplicationConfig("dubbo-demo-api-provider"));
+				service.setRegistry(new RegistryConfig("zookeeper://192.168.0.167:2181"));
+				service.setInterface(DemoService.class);
+				service.setRef(new DemoServiceImpl());
+				service.export();
+			}
+		}.start();
+		LockSupport.park();
+	}
 }
