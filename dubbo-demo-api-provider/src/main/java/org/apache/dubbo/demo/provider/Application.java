044853307c5c37808c419147035e043883c0d72a
297513458 <297513458@qq.com>
2019-05-08 16:05:26 +0800

    delete

--- dubbo-demo-api-provider/src/main/java/org/apache/dubbo/demo/provider/Application.java
+++ dubbo-demo-api-provider/src/main/java/org/apache/dubbo/demo/provider/Application.java
@@ -6,7 +6,6 @@ import org.apache.dubbo.config.ApplicationConfig;
 import org.apache.dubbo.config.RegistryConfig;
 import org.apache.dubbo.config.ServiceConfig;
 import org.apache.dubbo.demo.DemoService;
-
 public class Application {
 	public static void main(String[] args) throws Exception {
 		new Thread() {
